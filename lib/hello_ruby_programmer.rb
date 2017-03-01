5b6e1a40fa66544f76066d29cab66b93e103aada
Rachel Walwood <walwoodr@gmail.com>
2017-03-01 19:40:59 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,3 @@
-def greeting
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+def greeting(name)
+  puts "Hi, #{name}! You are now a fancy Ruby programmer in the making. Congratulations!"
 end
