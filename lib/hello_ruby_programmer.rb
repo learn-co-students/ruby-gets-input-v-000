c880f3bb5d169190d497ca4b7aaa5aea6e16f072
Natasha Holmes <natashanholmes@gmail.com>
2017-09-24 01:07:09 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,4 @@
-def greeting
+def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  puts "Please enter your name so that we can greet you more personally:"
 end
