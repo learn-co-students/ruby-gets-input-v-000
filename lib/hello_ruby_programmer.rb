0ba97bd5c02ff6154ff55d1952ebb46c15a4855c
Humza Joseph <humza1born@gmail.com>
2018-02-01 17:56:56 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,3 @@
 def greeting(name)
-  puts "What's happening #{name}! Welcome to the wonderful world of Ruby programming."
+  puts "What's happening, #{name}! Welcome to the wonderful world of Ruby programming."
 end
