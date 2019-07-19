5f07de2693578ea4df8277e6c465421180590126
Janan Akkad <jananakkad@hotmail.co.uk>
2019-07-19 20:19:47 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,6 @@
 puts "Hi! Welcome to the wonderful world of Ruby programming."
 puts "Please enter your name so that we can greet you more personally:"
-gets = "janan"
+name = gets
 
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
